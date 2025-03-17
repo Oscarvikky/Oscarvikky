720bfd019d776a7ceeca354f7c4355b204621623
unknown <Oscarvikky>
2025-03-17 14:16:39 +0000

    fix : vercel deploy issue

--- Config/dbConnnect.js
+++ Config/dbConnnect.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDb = async() =>{
-    connectionString = process.env.CONNECTINGSTRINGS;
-    try {
-       const connect = await mongoose.connect(connectionString)
-       if(connect){
-        console.log("e don connect")
-       }else{
-        console.log("cant connect");
-       }
-    } catch (error) {
-        console.log("internal server error", error);
+const connectDb = async () => {
+  connectionString = process.env.CONNECTINGSTRINGS;
+  try {
+    const connect = await mongoose.connect(connectionString);
+    if (connect) {
+      console.log("e don connect");
+    } else {
+      console.log("cant connect");
     }
-}
-module.exports = connectDb
+  } catch (error) {
+    console.log("internal server error", error);
+  }
+};
+module.exports = connectDb;
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
- const express = require("express")
-const connectDb = require("./Config/dbConnnect")
- const app = express()
- const env = require("dotenv").config()
- const UserRouter = require("./Routes/UserRoute")
- const productRoute = require("./Routes/Productroute")
- const cors= require("cors")
-const ErrorHandler = require("./Middleware/ErrorHandler")
+const express = require("express");
+const connectDb = require("./Config/dbConnnect");
+const app = express();
+const env = require("dotenv").config();
+const UserRouter = require("./Routes/UserRoute");
+const productRoute = require("./Routes/Productroute");
+const cors = require("cors");
+const ErrorHandler = require("./Middleware/ErrorHandler");
 
-
-
- app.use(express.json())
- app.use(express.urlencoded({extended:true}))
- app.use(cors({origin: "*"}))
- app.use(ErrorHandler)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+app.use(ErrorHandler);
 
 //  console.log("working free")
- connectDb()
- app.use("/Api/Users", UserRouter )
- app.use("/Api/Products", productRoute )
-  const port = process.env.PORT || 4000
-app.listen(port,()=>{
-  console.log(`app running on port ${port}`)
-})
+app.get((req, res) => {
+  app.send("welcome to dashboard");
+});
+connectDb();
+app.use("/Api/Users", UserRouter);
+app.use("/Api/Products", productRoute);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
